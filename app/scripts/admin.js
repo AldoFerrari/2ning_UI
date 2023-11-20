099bdcfef30885e9770bbb2b134c61c691eba1a8
Aldo <aldoferrari.it@gmail.com>
2023-11-20 15:59:46 +0100

    disbled lighter

--- app/scripts/admin.js
+++ app/scripts/admin.js
@@ -4,5 +4,20 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+const exampleModal = document.getElementById('approveDelete')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever');
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
 
-    
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('#recipient-name') 
+
+     modalTitle.textContent = `${recipient}`
+ 
+  })
+}
